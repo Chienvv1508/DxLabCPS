03c846f7bb84ba736b1be9c3e88d363c37a745a6
DucNT11042002 <101250360+DUCYT@users.noreply.github.com>
2025-03-15 22:24:51 +0700

    Update AccountController

--- DxLabCoworkingSpace.Core/DTOs/SlotGenerationRequest.cs
+++ DxLabCoworkingSpace.Core/DTOs/SlotGenerationRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,15 @@ namespace DxLabCoworkingSpace
 {
     public class SlotGenerationRequest
     {
+        //[Required(ErrorMessage = "StartTime là bắt buộc!")]
+        //[RegularExpression(@"^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$", ErrorMessage = "StartTime sai định dạng thời gian!")]
         public string? StartTime { get; set; }
+
+        //[Required(ErrorMessage = "EndTime là bắt buộc!")]
+        //[RegularExpression(@"^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$", ErrorMessage = "EndTime sai định dang thời gian!")]
         public string? EndTime { get; set; }
+
+        //[Range(0, int.MaxValue, ErrorMessage = "BreakTime phải là số không âm!")]
         public int? BreakTime { get; set; }
     }
 }
--- DxLabCoworkingSpace.Core/DTOs/UpdateRoleRequest.cs
+++ DxLabCoworkingSpace.Core/DTOs/UpdateRoleRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace DxLabCoworkingSpace
 {
     public class UpdateRoleRequest
     {
+        [Required(ErrorMessage = "RoleName không được để trống.")]
         public string RoleName { get; set; } = null!;
     }
 }
